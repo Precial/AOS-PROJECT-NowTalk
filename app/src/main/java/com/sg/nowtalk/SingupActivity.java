87a91bfaa604164a9697884dbff14835d75795e1
DESKTOP-2F6MTUK\SG <ggrty10@gmail.com>
2020-11-03 23:48:30 +0900

    회원가입 계정 정보 DB에 등록 완료

--- app/src/main/java/com/sg/nowtalk/SingupActivity.java
+++ app/src/main/java/com/sg/nowtalk/SingupActivity.java
@@ -13,6 +13,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.sg.nowtalk.model.UserModel;
 
 public class SingupActivity extends AppCompatActivity {
 
@@ -46,7 +48,11 @@ public class SingupActivity extends AppCompatActivity {
                         .addOnCompleteListener(SingupActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                UserModel userModel = new UserModel();
+                                userModel.userName = name.getText().toString();
 
+                                String uid = task.getResult().getUser().getUid();
+                                FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
                               
 
                             }
---
+++ app/src/main/java/com/sg/nowtalk/model/UserModel.java
@@ -0,0 +1,5 @@
+package com.sg.nowtalk.model;
+
+public class UserModel {
+    public String userName;
+}
